62dbb9a3e41ba609c6ab00a41cec8bd558bdf7e6
yunity29 <tanak0der@gmail.com>
2022-06-15 23:50:32 +0900

    スタイル設定を変更

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,11 +2,6 @@ import { defineNuxtConfig } from 'nuxt';
 
 export default defineNuxtConfig({
   srcDir: 'src/',
-  // buildModules: [
-  //   // Simple usage
-  //   '@nuxtjs/eslint-module',
-
-  //   // With options
-  //   ['@nuxtjs/eslint-module', { /* module options */ }],
-  // ],
+  buildModules: [
+  ],
 });
